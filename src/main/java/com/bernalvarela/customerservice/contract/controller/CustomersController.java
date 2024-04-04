d31ca3882f72931b2ffb88e662bc9f77ceb73123
Bernal Varela <bernal.varela@agatatechnology.es>
2024-04-04 12:22:31 +0200

    Remove unused import

--- src/main/java/com/bernalvarela/customerservice/contract/controller/CustomersController.java
+++ src/main/java/com/bernalvarela/customerservice/contract/controller/CustomersController.java
@@ -13,7 +13,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
